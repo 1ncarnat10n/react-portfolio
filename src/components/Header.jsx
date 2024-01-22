f4974f23bd52f2e45c8ed8488d35785625ba3813
BasedAtlas <tsaiwilliam888@gmail.com>
2024-01-22 15:35:27 -0800

    Added components and styles, basic layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// Imports
+import Navigation from "./Navigation"
+import '../../style/header.css'
+
+// Header component
+const Header = () =>{
+    return(
+        <>
+        <div className='header'>
+            <h1>William Tsai</h1>
+        </div>
+         <Navigation/>
+         </>
+    )
+}
+ export default Header
